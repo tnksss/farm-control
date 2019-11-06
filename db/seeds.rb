688c50752ffb3ae236597f8dfa4bc043b449d1aa
Valdinei Moraes <valdineimoraes@alunos.utfpr.edu.br>
2019-11-05 21:15:57 -0300

    Setup admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create_with(name: 'Administrador', password: '123456')
+     .find_or_create_by!(email: 'admin@admin.com')
